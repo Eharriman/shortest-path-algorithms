c5469c80ad396432efa63af5d213e19443c235ad
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-08-16 11:57:06 -0400

    fixed direction

--- src/core/graph.py
+++ src/core/graph.py
@@ -10,8 +10,20 @@ class Graph:
         self.adj[u].append((v, weight))
         self.nodes.add(u)
         self.nodes.add(v)
-        if not self.directed
+        if not self.directed:
             self.adj[v].append((u, weight))
 
     def get_neighbours(self, u):
         return self.adj[u]
+
+
+
+graph1 = Graph(directed=True)
+
+graph1.add_edge('A', 'B', 3)
+graph1.add_edge('A', 'C', 5)
+graph1.add_edge('B', 'C', 6)
+graph1.add_edge('C', 'D', 2)
+
+for neighbour, weight in graph1.get_neighbours('A'):
+    print(f"Edge A -> {neighbour}, weight = {weight}")
