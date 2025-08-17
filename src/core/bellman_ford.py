e7063fd3c0420f3841d6afe34acf3c74f053f9c0
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-08-17 14:28:36 -0400

    negative cycle exception class

--- src/core/bellman_ford.py
+++ src/core/bellman_ford.py
@@ -2,16 +2,28 @@ import sys
 from typing import Tuple
 from src.core.graph import Graph
 
+class NegativeWeightCycleError(Exception):
+    def __init__(self, cycle):
+        self.cycle = cycle
+        super().__init__(f"Graph contains negative-weight cycle: {cycle}")
+
 def bellman_ford(graph: Graph, src: str) -> Tuple[dict, dict]:
     # distance/prev ptr. dictionaries
     dist = {node: float('inf') for node in graph.nodes}
     prev = {node: None for node in graph.nodes}
 
     # Queue for unvisited nodes
-    Q = set(graph.nodes)
+    #Q = set(graph.nodes)
 
     # initial conditions for source node
     dist[src] = 0
+
+    for _ in range(len(graph.nodes) - 1):
+        for u in graph.nodes:
+            for v, weight in graph.adj[u]:
+                if dist[u] + weight < dist[v]:
+                    dist[v] = dist[u] + weight
+                    prev[v] = u
 
 
     return dist, prev
