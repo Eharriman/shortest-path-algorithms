4da0a23c6b858b5d827606336a1dea27e42a3224
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-08-18 16:11:16 -0400

    Base case Alg. 2

--- src/core/dmmsy.py
+++ src/core/dmmsy.py
@@ -1,4 +1,5 @@
 from src.core.graph import Graph
+import heapq
 
 class DMMSY:
     def __init__(self, graph, k, t):
@@ -29,3 +30,41 @@ class DMMSY:
         S = {src}
 
         pass
+
+    def base_case(self, B, S):
+        """
+
+        :param B:
+        :param S:
+        :return:
+        """
+
+        assert len(S) == 1
+        x = next(iter(S))
+
+        U0 = {x}
+        # Binary heap w/ singleton element and distance. See Algorithm 2 line 3.
+        heap = [(self.bd[x], x)]
+
+        visited = set()
+
+        while heap and len(U0) < self.k + 1:
+            d_u, u = heapq.heappop(heap) # Line 5. Extract the minimum
+
+            if u in visited:
+                continue
+            visited.add(u)
+            U0.add(u)
+
+            for v, w_uv in self.graph.get_neighbours(u):
+                new_dist = self.bd[u] + w_uv
+                if new_dist < self.bd[v] and new_dist < B:
+                    self.bd[v] = new_dist
+                    heapq.heappush(heap, (self.bd[v], v))
+
+        if len(U0) <= self.k:
+            return B, U0
+        else:
+            B_prime = max(self.bd[v] for v in U0)
+            U = {v for v in U0 if self.bd[v] < B_prime}
+            return B_prime, U
