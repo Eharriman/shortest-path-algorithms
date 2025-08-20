5a9476e69747608743e79aaf11d2c648d5a727d9
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-08-20 16:58:13 -0400

    Insert on boundary heap method

--- src/core/boundary_heap.py
+++ src/core/boundary_heap.py
@@ -17,4 +17,28 @@ class BoundaryHeap:
         :param B: This is the upper bound on all involved values
         :return:
         """
-        pass
+        self.heap = []
+        self.entry_map = {}
+        self.counter = 0
+        self.bound = B
+        self.deleted.clear()
+        #pass
+
+    def insert(self, node, value):
+        """
+        Key pair <a,b>: check if a is in heap, and if so repalce if b < b'.
+        <a,b> then placed in D1 depending on the bound?
+        :param node:
+        :param value:
+        :return:
+        """
+        if node in self.entry_map:
+            old_val, _ = self.entry_map[node]
+            if value >= old_val:
+                return
+            self.deleted.add((old_val, node))
+
+        entry = (value, self.counter, node)
+        self.entry_map[node] = (value, self.counter)
+        heapq.heappush(self.heap, entry)
+        self.counter += 1
