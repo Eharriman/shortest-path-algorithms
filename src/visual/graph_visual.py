b07ed263dbc859e91ccc60828fe33b65610a3eba
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-08-24 11:39:40 -0400

    Graph visualizer class

---
+++ src/visual/graph_visual.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from src.core.graph import Graph
+
+class GraphVisualizer:
+    def __init__(self, graph):
+        self.graph = graph
+        self.G = nx.DiGraph() if graph.directed else nx.Graph()
+        self._convert_graph()
+
+    def _convert_graph(self):
+        for u in self.graph.nodes:
+            self.G.add_node(u)
+        for u in self.graph.nodes:
+            for v, w in self.graph.get_neighbours(u):
+                self.G.add_edge(u, v, weight=w)
+
+
