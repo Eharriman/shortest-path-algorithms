e08b6220abd94529c54d819cfa5f17335e3ceeb9
Ethan Harriman <61565123+Eharriman@users.noreply.github.com>
2025-08-29 10:00:25 -0400

    Reverted dijkstra to original

--- src/core/dijkstra.py
+++ src/core/dijkstra.py
@@ -1,10 +1,9 @@
 import sys
 from typing import Tuple
 from src.core.graph import Graph
-from src.visual.graph_visual import GraphVisualizer
 
 
-def dijkstra(graph: Graph, src: str, visualizer: GraphVisualizer) -> Tuple[dict, dict]:
+def dijkstra(graph: Graph, src: str) -> Tuple[dict, dict]:
     # distance/prev ptr. dictionaries
     dist = {node: float('inf') for node in graph.nodes}
     prev = {node: None for node in graph.nodes}
@@ -15,17 +14,10 @@ def dijkstra(graph: Graph, src: str, visualizer: GraphVisualizer) -> Tuple[dict,
     # initial conditions for source node
     dist[src] = 0
 
-    visited = set()
-
     while Q:
         # Retrieves current shortest distance for node
         u = min(Q, key=lambda n: dist[n])
         Q.remove(u)
-        # Tracking visited node for visualization
-        visited.add(u)
-
-        highlight_nodes = visited.copy()
-        highlight_edges = set()
 
         # Update length based on neighbour distance
         for neighbour, weight in graph.adj[u]:
@@ -33,14 +25,5 @@ def dijkstra(graph: Graph, src: str, visualizer: GraphVisualizer) -> Tuple[dict,
             if curr_length < dist[neighbour]:
                 dist[neighbour] = curr_length
                 prev[neighbour] = u
-                highlight_edges.add((u, neighbour))
-
-        visualizer.draw(
-            highlight_nodes=highlight_nodes,
-            highlight_edges=highlight_edges,
-            title=f"Dijkstra visiting: {u}, distances: {dist}"
-        )
 
     return dist, prev
-
-
